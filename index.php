8eb5394ffe8338eb544407c5990d8de4b8692a59
KincsesErik <kincseserik3@gmail.com>
2023-01-24 10:03:57 +0100

    reg

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Game Haven</title>
     <link rel="icon" type="image/x-icon" href="images/gamehaveen.png">
@@ -47,7 +48,7 @@
     </nav>
 
 </head>
-<body style="background-color:#4618AC;">
+
 
 
 
---
+++ register.php
@@ -0,0 +1,21 @@
+<?php 
+session_start(); 
+include "db_inc.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])
+    && isset($_POST['name']) && isset($_POST['re_password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	$re_pass = validate($_POST['re_password']);
+	$name = validate($_POST['name']);
+
+	$user_data = 'uname='. $uname. '&name='. $name;
