8ba9dceb67288c60a1052a6212c4de5f7e9e9e66
KincsesErik <kincseserik3@gmail.com>
2023-02-14 13:05:50 +0100

    játék kártyák

--- View/Index/Main.php
+++ View/Index/Main.php
@@ -1,7 +1,40 @@
 <?php
     require_once '../Layout/header.php';
 ?>
-
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-12 col-md-4">
+            <div class="card">
+                <img alt="Card image cap" class="card-img-top" src="../../images\gamehaven.png">
+                <div class="card-body">
+                    <h5 class="card-title">játék neve</h5>
+                    <p class="card-text"></p>
+                    <a class="btn btn-primary" href="games.php">Megnézem</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <div class="card">
+                <img alt="Card image cap" class="card-img-top" src="../../images\gamehaven.png">
+                <div class="card-body">
+                    <h5 class="card-title">játék neve</h5>
+                    <p class="card-text"></p>
+                    <a class="btn btn-primary" href="games.php">Megnézem</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-4">
+            <div class="card">
+                <img alt="Card image cap" class="card-img-top" src="../../images\gamehaven.png">
+                <div class="card-body">
+                    <h5 class="card-title">játék neve</h5>
+                    <p class="card-text"></p>
+                    <a class="btn btn-primary" href="games.php">Megnézem</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
     require_once '../Layout/footer.php';
