e31cbb5880d73385e0d2d9596d93765889b8ecb8
KincsesErik <kincseserik3@gmail.com>
2023-02-15 10:00:10 +0100

    slider + slider css

--- View/Index/Main.php
+++ View/Index/Main.php
@@ -1,6 +1,61 @@
 <?php
     require_once '../Layout/header.php';
 ?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="sliderstyle.css">
+	<title>CSS slideshow</title>
+</head>
+  <body>
+	<div class="slider">
+	<ul>
+		<li>
+			<div class="slidetitle"> 
+				<span>Kung-Fu Panda</span>
+			</div>
+			<a href='#'>
+				<img src="https://i.imgur.com/lwYJ4rx.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="slidetitle"> 
+				<span>Toy Story</span>
+			</div>
+			<a href='#'>
+				<img src="https://i.imgur.com/BjB65TS.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="slidetitle"> 
+				<span>Wall-E</span>
+			</div>
+			<a href='#'>
+				<img src="https://i.imgur.com/nyettOe.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="slidetitle"> 
+				<span>Up</span>
+			</div>
+			<a href='#'>
+				<img src="https://i.imgur.com/WBTntRU.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="slidetitle"> 
+				<span>Calisto Protocol</span>
+			</div>
+			<a href='#'>
+				<img src="../../images\callisto.jpg"/>
+			</a>
+		</li>
+	</ul>
+	</div>
+		
+</body>
+</html>
 <div class="container">
     <div class="row mt-5">
         <div class="col-12 col-md-4">
