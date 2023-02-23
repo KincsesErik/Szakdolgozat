b2f26e8e7fc993604b02cd2655fae99248793d09
KincsesErik <kincseserik3@gmail.com>
2023-02-23 08:34:44 +0100

    adatbázis

--- db_inc.php
+++ db_inc.php
@@ -1,15 +1,44 @@
 <?php
 
 class DataBase{
-$sname= "localhost";
-$uname= "c31kincsesdbu";
-$password = "gyevFtJ9TX@E";
-$db_name = "c31kincsesdb";
+private $sname= "localhost";
+private $uname= "c31kincsesdbu";
+private $password = "gyevFtJ9TX@E";
+private $db_name = "c31kincsesdb";
 
+private $conn;
 
-$conn = mysqli_connect($sname, $uname, $password, $db_name);
+function __construct(){
+	// Create connection
+	$conn = new mysqli($this->sname, $this->uname, $this->password, $this->db_name);
 
-if (!$conn) {
-	echo "Sikertelen csatlakozás!";
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
 	}
+	$this->conn = $conn;
+}
+
+public function dbselect($sql){
+
+	if($result = $this->conn->query($sql)){
+	if ($result->num_rows > 0){
+		return $result;
+	}
+	else{
+		return NULL;
+	}
+	}
+	elseif($this->conn->error){
+		die("SQL error: ". $this->conn->error);
+	}
+}
+
+public function dbinsert($sql){
+	if ($this->conn->query($sql) === TRUE) {} 
+	else {
+		echo "Error: " . $sql . "<br>" . $this->conn->error;
+	}
+		
+}
 }
