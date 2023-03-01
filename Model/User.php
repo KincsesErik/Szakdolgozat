e01a6455dcb70b5308db91b4bf55c9d3792821b8
KincsesErik <kincseserik3@gmail.com>
2023-03-01 08:53:54 +0100

    Update User.php

--- Model/User.php
+++ Model/User.php
@@ -14,9 +14,33 @@ class Felhasznalo{
         $this->db=$db;
     }
 
+    public function checkLogin($username, $password){
+        $sql = "SELECT * FROM felhasznalo WHERE felhasznalonev = '".$_POST['uname']."'";
+        if($result = $this->db->dbselect($sql)){
+            if($row = $result->fetch_assoc()){
+                if($row['felhasznalonev'] == $uname){
+                    if($row['jelszo'] == md5($password)){
+                        $loginResult = 2 ;//successful login
+                        $_SESSION["felhasznalonev"] = $row['felhasznalonev'];
+                        $_SESSION["id"] = $row['id'];
+                    }
+                    else{
+                        $loginResult = 1 ;//incorrect password
+                    }
+                }
+                else{
+                    echo "Rossz felhasznalonév.";
+                }
+            }
+        }
+        else{
+            $loginResult = 0; //incorrect username
+        }
+        return $loginResult;
+    }
     
     public function registerUser($uname, $password, $name){
-        $sql = "INSERT INTO user (id, felhasznalonev, jelszo, nev) VALUES (NULL,'$uname','".md5($password)."','$name',1)";
+        $sql = "INSERT INTO user (id, felhasznalonev, jelszo, nev) VALUES (NULL,'$uname','".md5($password)."','$name')";
         $result = $this->db->dbinsert($sql);
     }
 }
