9c8da6d3cf2aa3a2aeb4dd70ccb0114ec9acbb9b
KincsesErik <112863476+KincsesErik@users.noreply.github.com>
2023-03-03 00:39:43 +0100

    kártya. + css + több kártya  betöltése

--- View/Main.php
+++ View/Main.php
@@ -7,7 +7,7 @@
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="assets\css\sliderstyle.css">
-	<title>CSS slideshow</title>
+	<title></title>
 </head>
   <body>
 	<div class="slider">
@@ -84,89 +84,96 @@
 
 <br>
 
-<h1>ㅤㅤㅤNeked Ajánlott <i style="font-size:24px" class="fa"title="Az ajánlások a felhasználói tevékenységi adatokon alapulnak, beleértve a termékmegtekintéseket, a kosárba való hozzáadásokat, a kívánságlista-feltöltéseket és a vásárlásokat. Bejelentkezéskor az ajánlások személyre szabottak; egyébként ugyanazon országból származó felhasználók adatain alapulnak">&#xf017;</i></h1>
+<h1>ㅤㅤㅤNeked Ajánlott <i style="font-size:24px" class="fa" title="Az ajánlások a felhasználói tevékenységi adatokon alapulnak, beleértve a termékmegtekintéseket, a kívánságlista-feltöltéseket. Bejelentkezéskor az ajánlások személyre szabottak; egyébként ugyanazon országból származó felhasználók adatain alapulnak">&#xf017;</i></h1>
 
 <div class="container">
-<div class="row mt-5">
-<div class="row">
-    <?php
-    for ($i = 0; $i < 5; $i++) {
-        echo '<div class="col-12 col-md-2">
-                <div class="card">
-                    <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
-                    <div class="card-body">
-                        <h5 class="card-title">játék neve</h5>
-                        <p class="card-text"></p>
-<fieldset class="rating">
-	<link rel="stylesheet" type="text/css" href="assets\css\rate.css">
-    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
-    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
-</fieldset>
-			</div>
-		</div>
-	</div>';
+    <link rel="stylesheet" type="text/css" href="assets\css\cards.css">
 
-	}
-?>
+    <div class="row mt-5">
+        <?php
+        for ($i = 0; $i < 4; $i++) {
+            echo '<div class="col-12 col-md-3 mb-4">
+                    <div class="card">
+                        <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
+                        <div class="card-body">
+                            <h5 class="card-title">Játék Neve</h5>
+                            <p class="card-text"></p>
+                            <fieldset class="rating">
+                                <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
+                                <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
+                                <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
+                                <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
+                                <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>';
+        }
+        ?>
+    </div>
 </div>
 
-	<br>
 
-	<div class="row">
-    <?php
-    for ($i = 0; $i < 5; $i++) {
-        echo '<div class="col-12 col-md-2">
-                <div class="card">
-				<link rel="stylesheet" type="text/css" href="assets\css\rate.css">
-                    <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
-                    <div class="card-body">
-                        <h5 class="card-title">játék neve</h5>
-                        <p class="card-text"></p>
-<fieldset class="rating">
-	<link rel="stylesheet" type="text/css" href="assets\css\rate.css">
-    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
-    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
-</fieldset>
-			</div>
-		</div>
-	</div>';
 
-	}
-?>
-</div>
+<hr>
 
 <br>
 
-<div class="row">
-    <?php
-    for ($i = 0; $i < 5; $i++) {
-        echo '<div class="col-12 col-md-2">
-                <div class="card">
-                    <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
-                    <div class="card-body">
-                        <h5 class="card-title">játék neve</h5>
-                        <p class="card-text"></p>
-<fieldset class="rating">
-	<link rel="stylesheet" type="text/css" href="assets\css\rate.css">
-    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
-    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
-</fieldset>
-			</div>
-		</div>
-	</div>';
+<h1>ㅤㅤㅤTOP játékok <i style="font-size:24px" class="fa" title="Javaslatok a felhasználók által legjobbra értékelt termékeken alapulna">&#xf017;</i></h1>
 
-	}
-?>
+<div class="container">
+    <link rel="stylesheet" type="text/css" href="assets\css\cards.css">
+
+    <div class="row mt-5">
+        <?php
+        for ($i = 0; $i < 8; $i++) {
+            echo '<div class="col-12 col-md-3 mb-4">
+                    <div class="card">
+                        <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
+                        <div class="card-body">
+                            <h5 class="card-title">Játék Neve</h5>
+                            <p class="card-text"></p>
+                            <fieldset class="rating">
+                                <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
+                                <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
+                                <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
+                                <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
+                                <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>';
+        }
+        ?>
+
+        <?php
+        if (isset($_GET['show_more'])) {
+            for ($i = 0; $i < 8; $i++) {
+                echo '<div class="col-12 col-md-3 mb-4">
+                        <div class="card">
+                            <img alt="Card image cap" class="card-img-top" src="assets\images\gamehaven.png">
+                            <div class="card-body">
+                                <h5 class="card-title">Játék Neve</h5>
+                                <p class="card-text"></p>
+                                <fieldset class="rating">
+                                    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" ></label>
+                                    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4"></label>
+                                    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3"></label>
+                                    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2"></label>
+                                    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1"></label>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>';
+            }
+        }
+        ?>
+
+    </div>
+
+    <button type="button" onclick="window.location.href='?show_more=true'" class="btn btn-primary"button style="display: block; margin: 0 auto;">További Kártyák Betöltése</button>
 </div>
+
 
 <?php
     include 'Layout/footer.php';
